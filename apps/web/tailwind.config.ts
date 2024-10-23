1141d9b04a110482386914fb8441ee303f1aca7d
Lazare Rossillon <lazrossi@student.42.fr>
2024-10-23 07:53:32 +0200

    playing around

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -9,12 +9,26 @@ export default {
   // those classes are included correctly.
   content: [...baseConfig.content, '../../packages/ui/src/*.{ts,tsx}'],
   presets: [baseConfig],
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //       sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-  //       mono: ["var(--font-geist-mono)", ...fontFamily.mono],
-  //     },
-  //   },
-  // },
+  theme: {
+    extend: {
+      fontSize: {
+        'base': '1.5rem',    // 24px
+        'lg': '1.75rem',     // 28px
+        'xl': '2rem',        // 32px
+        '2xl': '2.5rem',     // 40px
+        '3xl': '3rem',       // 48px
+        '4xl': '3.75rem',    // 60px
+      },
+      lineHeight: {
+        'normal': '1.6',    // Increase line height for better readability
+      },
+      spacing: {
+        '1': '0.5rem',
+        '2': '1rem',
+        '3': '1.5rem',
+        '4': '2rem',
+        // ... and so on
+      },
+    },
+  },
 } satisfies Config;
