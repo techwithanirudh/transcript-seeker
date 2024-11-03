8fe1709b27e4d05e0cfeddba3c0c83b6c8589b07
Lazare Rossillon <lazrossi@student.42.fr>
2024-11-03 15:54:40 +0100

    feat: POC tauri app

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,13 @@
-import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import checker from 'vite-plugin-checker';
 
 // @ts-ignore
 export default defineConfig(() => {
-  const CLIENT_PORT: number = Number(process.env.VITE_CLIENT_PORT) || 5173;
+  // const CLIENT_PORT: number = Number(process.env.VITE_CLIENT_PORT) || 5173;
+  // always use port 5173 as tauri expects a fixed port
+  const CLIENT_PORT: number = 5173;
   const CLIENT_HOST: string = process.env.VITE_CLIENT_HOST || 'localhost';
 
   return {
@@ -17,9 +19,14 @@ export default defineConfig(() => {
       }),
     ],
     server: {
+      // Tauri expects a fixed port, fail if that port is not available
+      strictPort: true,
+
       port: CLIENT_PORT,
-      host: CLIENT_HOST,
+      host: CLIENT_HOST || false,
     },
+    // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+    envPrefix: ['VITE_', 'TAURI_ENV_*'],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
@@ -27,5 +34,13 @@ export default defineConfig(() => {
       },
     },
     optimizeDeps: { exclude: ['@electric-sql/pglite', '@meeting-baas/ui'] },
+    build: {
+      // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+      target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari14',
+      // don't minify for debug builds
+      minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+      // produce sourcemaps for debug builds
+      sourcemap: !!process.env.TAURI_ENV_DEBUG,
+    },
   };
 });
